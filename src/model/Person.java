c3875b6a4e1e70e4ebe2a46df5bb528c8f59b4ca
Torsa06 <otorsamondal92@gmail.com>
2026-03-28 23:17:18 +0530

    Add Person class with basic attributes

---
+++ src/model/Person.java
@@ -0,0 +1,25 @@
+package model;
+
+public abstract class Person {
+
+    private String id;
+    private String name;
+
+    public Person(String id, String name) {
+        this.id   = id;
+        this.name = name;
+    }
+
+    public String getId()   { return id; }
+    public String getName() { return name; }
+
+    public void setId(String id)     { this.id   = id; }
+    public void setName(String name) { this.name = name; }
+
+    public abstract String getDetails();
+
+    @Override
+    public String toString() {
+        return getDetails();
+    }
+}
